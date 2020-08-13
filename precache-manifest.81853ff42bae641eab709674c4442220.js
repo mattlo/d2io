d25252fa400d3c5bba48da2280e949ea22233540
Matt Lo <mattlo.developer@gmail.com>
2020-08-13 15:17:18 -0500

    Updates

---
+++ precache-manifest.81853ff42bae641eab709674c4442220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e993972d4cb594c55eb56311b9144b1",
+    "url": "/d2io/index.html"
+  },
+  {
+    "revision": "45173e47dad4057ada82",
+    "url": "/d2io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57067ccc81ee4dc9e48b",
+    "url": "/d2io/static/js/2.124dfd53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/d2io/static/js/2.124dfd53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45173e47dad4057ada82",
+    "url": "/d2io/static/js/main.1eed9df9.chunk.js"
+  },
+  {
+    "revision": "bdfa71bad3796d4fa5eb",
+    "url": "/d2io/static/js/runtime-main.9363fe62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/d2io/static/media/logo.5d5d9eef.svg"
+  }
+]);
